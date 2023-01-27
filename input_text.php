83cfcafcea028f34cc440a2d0e34a366f6ede3d8
Агаджанян Владимир Владикович <vladimir.agadzhanyan@eriell.com>
2023-01-27 11:14:10 +0500

    add button

--- input_text.php
+++ input_text.php
@@ -21,11 +21,12 @@
     <h1>Введите автора</h1>
 
     <form method="post" action="input_text.php">
-    
-        <label>Автор: </label><input type="text" size="20"> <input type="submit" value="Добавить">
-        <label>Текст: </label><input type="textaria" size="20"> <input type="submit" value="Добавить">
-        <label>Email: </label><input type="textaria" size="20"> <input type="submit" value="Добавить">
-    
+        <label>Автор: </label><input type="text" size="20" value="Добавить">
+        <label>Текст: </label><input type="textaria" size="20" value="Добавить">
+        <label>Email: </label><input type="textaria" size="20" value="Добавить">
+        <button type="submit">
+            Отправить
+        </button>
     </form>
     
     </body>
